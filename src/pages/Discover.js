d7a476dbb7f6cfaa439e174602c8c93a252630fa
myches <andorfulmichaelebo@gmail.com>
2023-04-09 11:57:56 +0000

    working on homepage responsiveness

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -5,7 +5,7 @@ import {MdStadium} from 'react-icons/md'
 
 function Discover() {
 
-
+  const [league, setLeague] = useState([]);
   const [standings, setStandings] = useState([]);
   const [fixtures, setFixtures] = useState([]);
   const [scorers, setScorers] = useState([]);
@@ -80,7 +80,17 @@ function Discover() {
   setStandings(standings.data.response[0].league.standings[0]);
   console.log(standings.data.response[0].league.standings[0])
   
-
+  const league = await axios.get(
+    'https://v3.football.api-sports.io/leagues?season=2022&id=39',
+    {
+      headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key': 'dcae5708933639629a715178535f464b',
+    
+    }}
+  );
+  setLeague(league.data.response);
+  console.log(league.data.response)
  } 
    
  useEffect(() => {
@@ -99,94 +109,72 @@ function Discover() {
       <div className='flex flex-col lg:flex-row'>
         <div className='flex flex-col  '>
    <h1 className='text-white lg:flex justify-center items-center'>FEATURED</h1>
-   {MOD.map((mod) => (
+   {MOD.map((mod) => {
+     const fixtureDate = new Date(mod.fixture.date);
+     const fixtureDateString = fixtureDate.toDateString();
+     const fixtureTimeString = fixtureDate.toLocaleTimeString();
+
+     return (
   <div >
-   <div className = 'w-[55vw] lg:h-[350px] border  px-4 lg:pt-4 bg-[url("./Ppl.png")]  bg-cover '>
+   <div className = 'lg:w-[55vw] w-[100%] lg:h-[350px] border  px-4 lg:pt-4 bg-[url("./Ppl.png")]  bg-cover '>
     
 <div className='flex justify-around items-center  border-b border-b border-cyan-300 '> <p>Premier league</p> <p className=''></p>{mod?.fixture?.venue?.name}</div>
-<div className='flex flex-col  justify-center p-4 mt-[150px] bg-gradient-to-r from-green-500 to-cyan-500'>
+<div className='flex flex-col  justify-center p-4 mt-[20vh]  bg-gradient-to-r from-green-500 to-cyan-500'>
 <div className='flex  justify-around  '>
-<div className='flex '> <p className='pt-2'>{mod?.teams?.home?.name}<p className='pl-4'> HOME</p></p><img src={mod?.teams?.home?.logo} className="ml-2  w-14 h-14 "/>  </div>
+<div className='flex '> <p className='lg:pt-2 '>{mod?.teams?.home?.name}<p className='lg:pl-4 hidden lg:block'> HOME</p></p><img src={mod?.teams?.home?.logo} className="lg:ml-2  lg:w-14 lg:h-14 w-7 h-7"/>  </div>
 
- <div className='flex flex-col justify-center items-center'><p className='border rounded-lg text-blue-900 bg-gray-300 px-2'>MATCH OF THE DAY</p>
-  <p>{mod?.fixture?.date}</p>
-  <p></p></div> 
+ <div className='flex flex-col justify-center items-center ml-2 text-center sm:text-left'><p className='border rounded-lg text-blue-900 bg-gray-300 px-2 text-sm '>MATCHDAY</p>
+  <p className='mt-4 w-full sm:w-auto'>{fixtureDateString}</p>
+  <p className='w-full sm:w-auto'> {fixtureTimeString}</p></div> 
 
-  <div className='flex '> <p className='pt-2'>{mod?.teams?.away?.name}<p className='pl-4'>AWAY</p></p> <img src={mod?.teams?.away?.logo} className="ml-2  w-14 h-14 "/></div>
+  <div className='flex '> <p className='lg:pt-2 '>{mod?.teams?.away?.name}<p className='lg:pl-4 hidden lg:block'>AWAY</p></p> <img src={mod?.teams?.away?.logo} className="lg:ml-2  lg:w-14 lg:h-14 w-7 h-7"/></div>
   </div></div>
 </div>
 
 
 
 
-</div> ))}</div>
-<div className='lg:mx-12 '>
+</div>     );
+      })} </div>
 
-   
-
-   
-     
-   <div  className=' text-white bg-gray-700 lg:p-4 border rounded-3xl border-none m-4 lg:h-[450px] h-[5%] sm:overflow-auto flex flex-row lg:flex-col' >
-   <h1 className='text-white lg:flex justify-center items-center '>FIXTURES</h1> 
-
-  {fixtures.map((fixture) => (
-   
-
-  <div className=' lg:p-4 m-4 border rounded-lg border-none bg-gray-950 text-sm'>
- <p className='flex justify-center items-center text-lime-300 '>{fixture?.league?.round}</p>
- <p className='flex justify-center items-center text-cyan-300'><p className='mr-4'><MdStadium  /></p>{fixture?.fixture?.venue?.name}</p> 
- <p className='flex justify-center items-center text-cyan-300'>{fixture?.fixture?.date}</p>
-  <div className='flex justify-center items-center text-lime-300'>
-<div className='flex flex-col p-4'><img src={fixture?.teams?.home?.logo} className=" w-8 h-8 "/> <p> {fixture?.teams?.home?.name}</p> </div> VS <div className='flex flex-col p-4'><img src={fixture?.teams?.away?.logo} className="  w-8 h-8 "/><p>{fixture?.teams?.away?.name}</p></div>
-
-  </div> </div>
-  ))} </div>
-  
-</div>
 
 </div>
 <div className='lg:flex-row justify-around  w-[87vw] flex flex-col '>
-<div className=' text-white lg:w-[60%] '> 
-<div className='  '>
-  
-  <h1 className='text-white flex justify-center items-center'>LEAGUE TABLE</h1>
-
-  <div className='bg-gray-950 pt-8 '> 
-<table className='table-auto m-4  '>
-      <thead className='  '>
-        <tr className='  text-lime-300 px-4 border-b border-cyan-300' >
-          <th className='px-4' >Rank</th>
-          <th className=''>Team</th>
-          <th className='px-4'>Played</th>
-          <th className='px-4'>Form</th>
-          <th className='px-4'>GD</th>
-          <th className='px-4'>PTS</th>
-        </tr>
-      </thead>
-      <tbody>
-  {standings.map((team) =>
-   
-      <tr key={team.team.id}className='border-b border-cyan-300 text-cyan-300' >
-        <td className='px-4'>{team?.rank}</td>
-        <td className='px-4 flex  items-center'>{team?.team?.name}<img src={team?.team?.logo} className=" w-7 m-2 border-none h-7 border rounded-full"/> </td>
-        <td className='px-4'>{team?.all?.played}</td>
-        <td className='px-4'>{team?.form}</td>
-        <td className='px-4'>{team?.goalsDiff}</td>
-        <td className='px-4'>{team?.points}</td>
-      </tr>
-   
-  )}
-</tbody>
-
-</table>
-
-
+<div className=' text-white lg:w-[60%] bg-gray-700  p-4 border rounded-3xl border-none '> 
+  <div className='  '>
+    <h1 className='text-white flex justify-center items-center'>LEAGUE TABLE</h1>
+    <div className='bg-gray-950 pt-8' style={{ overflowX: 'auto' }}>
+      <table className='table-auto m-4'>
+        <thead className='  '>
+          <tr className='  text-lime-300 px-4 border-b border-cyan-300' >
+            <th className='px-4' >Rank</th>
+            <th className=''>Team</th>
+            <th className='px-4'>Played</th>
+            <th className='px-4'>Form</th>
+            <th className='px-4'>GD</th>
+            <th className='px-4'>PTS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {standings.map((team) => (
+            <tr key={team.team.id} className='border-b border-cyan-300 text-cyan-300' >
+              <td className='px-4'>{team?.rank}</td>
+              <td className='px-4 flex items-center'>
+                {team?.team?.name}
+                <img src={team?.team?.logo} className="w-7 m-2 border-none h-7 border rounded-full"/>
+              </td>
+              <td className='px-4'>{team?.all?.played}</td>
+              <td className='px-4'>{team?.form}</td>
+              <td className='px-4'>{team?.goalsDiff}</td>
+              <td className='px-4'>{team?.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 
-</div>
-
-
-</div>
 
 <div className='flex flex-col'>
 <div className=' text-white mt-8 bg-gray-700 p-4 border rounded-3xl border-none'>
