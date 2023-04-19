043af8df111d3459a3b681661d589dee7b27df6e
myches <andorfulmichaelebo@gmail.com>
2023-04-19 20:31:07 +0000

    worken n the responsiveness of the sidebar

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -6,35 +6,37 @@ import logo from './logo.jpeg'
 
 
 
+
 function Sidebar () {
 
 
 
 
 
- return (
-    <div >
- <div className="w-[10vw] h-[100vh]  bg-gray-900 flex flex-col  items-center text-white ">
+  return (
+     <div >
+  <div className="w-[10vw] h-[100%]  bg-gray-900 flex flex-col  items-center text-white ">
+  
+  <p > <img src={logo} className="ml-2  w-14 h-14 border rounded-full  mt-8"/></p>
+          <p className="pt-28 pb-8" >
+             <Link to="/">
+               Discover
+             </Link>
+           </p>
+           <p className="pb-8">
+             <Link to="/Highlights">
+              Highlights
+             </Link>
+           </p>
+           <p className="pb-8">
+             <Link to="/Stats">
+             Statistics
+             </Link>
+           </p>
+   </div></div>
+     
+  )
+ }
  
- <p > <img src={logo} className="ml-2  w-14 h-14 border rounded-full  mt-8"/></p>
-         <p className="pt-28 pb-8" >
-            <Link to="/">
-              Discover
-            </Link>
-          </p>
-          <p className="pb-8">
-            <Link to="/Highlights">
-             Highlights
-            </Link>
-          </p>
-          <p className="pb-8">
-            <Link to="/Stats">
-            Statistics
-            </Link>
-          </p>
-  </div></div>
-    
- )
-}
-
-export default Sidebar
+ export default Sidebar
+ 
